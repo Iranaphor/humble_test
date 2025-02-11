3dac7d0ab6aad7234d5e578d40cc6e99e67493f4
James R Heselden <primordia@live.com>
2025-02-11 11:14:49 +0000

    package renamed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ros2_python_template
+# humble_test
 1. create repo from template
 2. git clone the new repo
 3. cd into the bash folder
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from glob import glob
 import os
 
-package_name = 'ros2_python_template'
+package_name = 'humble_test'
 pkg = package_name
 
 setup(
